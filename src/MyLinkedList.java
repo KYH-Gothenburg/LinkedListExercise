eadcf4192b60fe068a0cb8ca89ed367364f728b1
Joakim Wassberg <joakim@arthead.se>
2021-11-11 16:03:24 +0100

    Added linked list methods

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,6 +1,7 @@
 public class MyLinkedList {
     private Node head;
     private Node tail;
+    private int size;
 
     /*
         Specialfall för samtliga metoder
@@ -10,19 +11,85 @@ public class MyLinkedList {
      */
 
     public void insertHead(int data) {
-
+        if(head == null) {
+            head = new Node(data);
+            tail = head;
+        }
+        else {
+            Node newNode = new Node(data);
+            newNode.setNext(head);
+            head = newNode;
+        }
+        size++;
     }
 
     public void insertTail(int data) {
+        if(tail == null) {
+            tail = new Node(data);
+            head = tail;
+        }
+        else {
+            tail.setNext(new Node(data));
+            tail = tail.getNext();
+        }
+        // When doing the actual insert
+        size++;
 
+    }
+
+    public void insertAt(int index, int data) {
+        // Är index större än size? Ge felmeddelande
+        // Är index 0? Kalla på insertHead
+        // Är index == size? Kalla på insertTail
+        // Vi är mitt i listan!
     }
 
     public int removeHead() {
-        return 0;
+        // Is list empty?
+        if(head == null) {
+            throw new IndexOutOfBoundsException("List is empty");
+        }
+        // When doing the actual remove
+        size--;
+        // Only one node in the list
+        if(head == tail) {
+            int value = head.getData();
+            head = null;
+            tail = null;
+            return value;
+        }
+        // There must be more than one node in the list
+        int value = head.getData();
+        head = head.getNext();
+        return value;
     }
 
     public int removeTail() {
-        return 0;
+        if(tail == null) {
+            throw new IndexOutOfBoundsException("List is empty");
+        }
+        // When doing the actual remove
+        size--;
+        if(head == tail) {
+            int value = tail.getData();
+            head = null;
+            tail = null;
+            return value;
+        }
+        // Make sure that tmp points at the node just before tail
+        int value = tail.getData();
+        Node tmp = head;
+        while(tmp.getNext() != tail) {
+            tmp = tmp.getNext();
+        }
+        tail = tmp;
+        tail.setNext(null);
+        return value;
+
+    }
+
+    public int size() {
+        return size;
     }
 
     public void printList() {
